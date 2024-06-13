425535033f19be673ab992cc1f4d4f2fc9e6e1b8
MarquezAlan <160274481+MarquezAlan@users.noreply.github.com>
2024-06-13 02:20:23 -0400

    Add files via upload

---
+++ salon-de-belleza/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/MainNavbar.vue'; // Importa el Navbar
+
+export default {
+  name: 'App',
+  components: {
+    Navbar // Utiliza el componente Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
